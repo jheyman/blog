ff6ee360fb1d5b6916b98007c79fb8937e854284
Julien Heyman <bidsomail at gmail.com>
2016-08-21 22:33:53 +0200

    no change

--- publish.sh
+++ publish.sh
@@ -1,8 +1,16 @@
 #! /bin/bash
 
+# build site, commit and push generated files to github blog page
+
 jekyll build
 cd publish/blog/
 rsync -arv ../../_site/ .
 git add . --all
 git commit -m "$1"
 git push -u origin gh-pages
+
+# Now commit & push source files themselves
+cd ../..
+git add . --all
+git commit -m "$1"
+git push -u origin master
